86d42aba0640fef650101ae2c4b3d09b56618ed8
Jonathan <joededeson@gmail.com>
2022-07-01 16:18:42 -0700

    Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Description: A program that prints all combinations of...
+ * ...two 2-dgit numbers
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i, j;
+	int a, b, c, d;
+
+	for (i = 0; i < 100; i++)
+	{
+		a = i / 10; /*doubles fnum*/
+		b = i % 10; /*singles fnum*/
+
+		for (j = 0; j < 1000; j++)
+		{
+			c = i / 100; /*doubles snum*/
+			d = j % 100; /*singles snum*/
+
+			if (a < c || (a == c && b < d))
+			{
+				putchar(a + '0');
+				putchar(b + '0');
+				putchar(32);
+				putchar(c + '0');
+				putchar(d + '0');
+
+
+				if (!(a == 9 && b == 8))
+				{
+					putchar(44);
+					putchar(32);
+				}
+
+			}
+		}
+	}
+	putchar(10);
+
+	return (0);
+
+}
