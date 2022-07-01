d7808a43c6a1026fa1bf2c49a30f289e20db2049
Jonathan <joededeson@gmail.com>
2022-07-01 13:30:34 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Description: A program that prints lowercase alphabets in reverse order
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'z';
+
+	while (c <= 'a')
+	{
+		putchar(c);
+		c--;
+	}
+	putchar('\n');
+
+	return (0);
+}
